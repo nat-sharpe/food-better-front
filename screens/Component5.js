cb124ef2ab1ee9393b7de382c9a8c9b2018b2dc8
Nat Sharpe <sharpe1890@gmail.com>
2018-10-05 13:15:38 -0400

    Adds QR reader to page

--- screens/Component5.js
+++ screens/Component5.js
@@ -1,50 +1,52 @@
-import React, {Component} from 'react';
-import {AppRegistry, Text, View, StyleSheet, TouchableOpacity} from 'react-native';
-import {QRscanner} from 'react-native-qr-scanner';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Alert, TouchableOpacity } from 'react-native';
+import { Constants, BarCodeScanner, Permissions } from 'expo';
 
-export default class Component2 extends Component{
+export default class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
+      hasCameraPermission: null,
       item1: {id: '9', message: 'Scan'},
       item2: {id: '4', message: 'Scan'},
       item3: {id: '7', message: 'Scan'},
     }
   }
 
-  pressHandler = (item) => {
-    const URL = 'http://foodbetter.fun:3000/scan';
-    fetch(
-      URL, 
-      {
-        method: 'POST',
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          id: this.state[item].id
-        })
-      }
-    ) 
-    .then(response => {
-      console.log(this.state[item])
-      response.json()
-      .then(data => {
-        console.log(data.status)
-        let status = data.status ? 'YES' : 'NO';
-        console.log(status)
-        this.setState({[item]: {id: this.state[item].id, message: status}});
-        console.log(this.state[item])
-      })
-    })
-    .catch(err => console.log(err));
+  componentDidMount() {
+    this._requestCameraPermission();
   }
 
-  render(){
-    return(
+  _requestCameraPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: status === 'granted',
+    });
+  };
+
+  _handleBarCodeRead = data => {
+    Alert.alert(
+      'Scan successful!',
+      JSON.stringify(data)
+    );
+  };
+
+  render() {
+    return (
       <View>
         <Text style={styles.myText}>Food Better</Text>
+        <View style={styles.scanner}>
+          {this.state.hasCameraPermission === null ?
+            <Text>Requesting for camera permission</Text> :
+            this.state.hasCameraPermission === false ?
+              <Text>Camera permission is not granted</Text> :
+              <BarCodeScanner
+                onBarCodeRead={this._handleBarCodeRead}
+                style={styles.scanner}
+              />
+          }
+        </View>
         <View style={styles.container}>
           <TouchableOpacity 
             style={styles.v1} 
@@ -78,11 +80,22 @@ export default class Component2 extends Component{
           </TouchableOpacity>
         </View>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
+  scanner: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  scanner: {
+    height: 200, 
+    width: 300
+  },
   myText: {
     color: 'white',
     backgroundColor: 'blue'
@@ -98,6 +111,4 @@ const styles = StyleSheet.create({
     margin: 3,
     marginTop: 6
   }
-})
-
-AppRegistry.registerComponent('Component2', () => Component2);
+});
